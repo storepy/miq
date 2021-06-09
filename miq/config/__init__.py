ebaf9e8d4dbb0470a4b563f5f7ba9d1a45f63dc8
Michaël <michael.gainyo@gmail.com>
2021-06-09 15:06:08 -0400

    conf:update

--- miq/config/__init__.py
+++ miq/config/__init__.py
@@ -1,14 +1,6 @@
 # import environ
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
-TEMPLATES_DIR = BASE_DIR / 'templates'
-CLIENT_DIR = BASE_DIR / 'client'
-BUILD_DIR = CLIENT_DIR / 'build'
 
 CORS_ORIGIN = None
-
 DEBUG = False
 
 """
