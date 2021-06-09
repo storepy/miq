13941947584a72be07c14aa1289cdd7f4e485a64
Michaël <michael.gainyo@gmail.com>
2021-06-09 12:49:12 -0400

    fix:package scaffolding

---
+++ setup.py
@@ -0,0 +1,22 @@
+
+from setuptools import setup
+
+setup(
+    name='miq',
+    version='1.0.0',
+    description='',
+    long_description='',
+    url='http://github.com/marqetintl/miq',
+    author='marqetintl',
+    author_email='michaelgainyo@gmail.com',
+    keywords='',
+    license='',
+    packages=['miq'],
+    install_requires=[
+        'django',
+    ],
+    # test_suite='nose.collector',
+    # tests_require=['nose'],
+    # include_package_data=True
+    zip_safe=False
+)
