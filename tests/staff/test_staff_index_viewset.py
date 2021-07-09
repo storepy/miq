4829dacde12c3dfc2d027ec7435df0afc4bd5383
Michaël <michael.gainyo@gmail.com>
2021-07-09 10:51:41 -0400

    update staff tests

---
+++ tests/staff/test_staff_index_viewset.py
@@ -0,0 +1,93 @@
+
+from django.urls import reverse_lazy
+
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from miq.models import Section
+
+from miq.tests.mixins import TestMixin
+
+path = reverse_lazy('miq:index-detail', args=['current'])
+
+
+class Mixin(TestMixin):
+    pass
+
+
+class TestIndexViewset(Mixin, APITestCase):
+    def setUp(self) -> None:
+        super().setUp()
+
+        self.site.save()
+        self.user = self.create_staffuser(self.username, self.password)
+        self.add_user_perm(self.user, 'change_index')
+
+        self.client.login(
+            username=self.username,
+            password=self.password
+        )
+
+    def test_on_section_add(self):
+        self.assertTrue(self.user.has_perm('miq.change_index'))
+        path = reverse_lazy('miq:index-section', args=['current'])
+
+        # Add section
+        r = self.client.post(path, data={}, format='json')
+        self.assertEqual(r.status_code, status.HTTP_201_CREATED)
+
+        index = self.site.index
+
+        # Retriev page sections
+        section_list_path = reverse_lazy('miq:section-list')
+        r = self.client.get(section_list_path, {'source': index.slug})
+        self.assertEqual(r.data.get('count'), 1)
+
+    def test_sections_is_readonly(self):
+        section = Section.objects.create(site=self.site)
+
+        r = self.client.patch(
+            path, data={'sections': [section.slug]}, format='json')
+        self.assertEqual(r.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(r.data.get('sections')), 0)
+
+    def test_index_partial_update(self):
+        self.assertTrue(self.user.has_perm('miq.change_index'))
+
+        title = 'Index 1'
+        r = self.client.patch(
+            path, data={'title': title}, format='json')
+
+        self.assertEqual(r.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.site.index.title, title)
+
+
+class TestIndexNotAuthUserViewset(Mixin, APITestCase):
+    def setUp(self) -> None:
+        super().setUp()
+
+        self.get_user()
+        self.site.save()
+        self.client.login(
+            username=self.username,
+            password=self.password)
+
+    def test_index_partial_update_no_perm(self):
+        self.user.is_staff = True
+        self.user.save()
+
+        self.assertFalse(self.user.has_perm('miq.change_index'))
+
+        r = self.client.patch(
+            path, data={'title': 'Index 1'}, format='json')
+        self.assertEqual(r.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_user_not_staff(self):
+        r = self.client.get(path)
+        self.assertEqual(r.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_user_not_auth(self):
+        self.client.logout()
+
+        r = self.client.get(path)
+        self.assertEqual(r.status_code, status.HTTP_302_FOUND)
---
+++ tests/staff/test_staff_page_viewset.py
@@ -0,0 +1,132 @@
+from django.urls import reverse_lazy
+
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from miq.models import Page, Section
+
+from miq.tests.mixins import TestMixin
+
+list_path = reverse_lazy('miq:page-list')
+
+
+class Mixin(TestMixin):
+
+    def get_detail_path(self, slug):
+        return reverse_lazy('miq:page-detail', args=[slug])
+
+
+class TestPageViewset(Mixin, APITestCase):
+    def setUp(self) -> None:
+        super().setUp()
+
+        self.user = self.create_staffuser(self.username, self.password)
+        self.add_user_perm(self.user, 'change_page')
+        self.client.login(
+            username=self.username,
+            password=self.password
+        )
+
+    def test_on_slug_update(self):
+        # All section slugs should update as well
+        pass
+
+    def test_on_section_add(self):
+        slug = Page.objects.create(
+            label='Section test page', site=self.site).slug
+        path = reverse_lazy('miq:page-section', args=[slug])
+
+        # No slug
+        r = self.client.post(path, data={}, format='json')
+        # Adds text section
+        # self.assertEqual(r.status_code, status.HTTP_400_BAD_REQUEST)
+
+        # Add section
+        r = self.client.post(path, data={'type': "MD"}, format='json')
+        self.assertEqual(r.status_code, status.HTTP_201_CREATED)
+        # self.assertIn(f'{section_slug}', r.data.get('sections'))
+
+        # update_sections_source
+        # section = Page.objects.get(slug=slug).sections.get(slug=section_slug)
+        # self.assertEqual(section.source, f'{slug}')
+
+        # Retriev page sections
+        section_list_path = reverse_lazy('miq:section-list')
+        r = self.client.get(section_list_path, {'source': slug})
+        self.assertEqual(r.data.get('count'), 2)
+
+    def test_sections_is_readonly(self):
+        slug = Page.objects.create(
+            label='Section test page', site=self.site).slug
+        path = self.get_detail_path(slug)
+
+        section = Section.objects.create(site=self.site)
+
+        r = self.client.patch(
+            path, data={'sections': [section.slug]}, format='json')
+        self.assertEqual(r.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(r.data.get('sections')), 0)
+
+    def test_partial_update(self):
+        slug = Page.objects.create(label='Old label', site=self.site).slug
+        path = self.get_detail_path(slug)
+
+        label = 'New label'
+        r = self.client.patch(
+            path, data={'label': label}, format='json')
+
+        self.assertEqual(r.status_code, status.HTTP_200_OK)
+        page = Page.objects.draft().get(slug=slug)
+        self.assertEqual(page.label, label)
+
+        r = self.client.patch(
+            path, data={'is_published': True}, format='json')
+        self.assertTrue(Page.objects.published().get(slug=slug).is_published)
+
+    def test_create(self):
+        self.add_user_perm(self.user, 'add_page')
+
+        r = self.client.post(list_path, data={}, format='json')
+        self.assertEqual(r.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertTrue(self.user.has_perm('miq.add_page'))
+
+        r = self.client.post(
+            list_path, data={'label': 'page label'}, format='json')
+        self.assertEqual(r.status_code, status.HTTP_201_CREATED)
+
+        self.assertFalse(r.data.get('is_published'))
+
+    def test_list(self):
+        Page.objects.create(label='Page', site=self.site)
+
+        self.assertFalse(self.user.has_perm('miq.view_page'))
+        r = self.client.get(list_path)
+        self.assertEqual(r.status_code, status.HTTP_403_FORBIDDEN)
+
+        self.add_user_perm(self.user, 'view_page')
+        self.refresh_user(self.user.username)
+        self.assertTrue(self.user.has_perm('miq.view_page'))
+
+        r = self.client.get(list_path)
+        self.assertEqual(r.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(r.data.get('results')), 1)
+
+
+class TestPageNotStaffViewset(Mixin, APITestCase):
+    def setUp(self) -> None:
+        super().setUp()
+
+        self.get_user()
+        self.client.login(
+            username=self.username,
+            password=self.password)
+
+    def test_user_not_staff(self):
+        r = self.client.get(list_path)
+        self.assertEqual(r.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_user_not_auth(self):
+        self.client.logout()
+
+        r = self.client.get(list_path)
+        self.assertEqual(r.status_code, status.HTTP_302_FOUND)
