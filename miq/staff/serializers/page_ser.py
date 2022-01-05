0ed2c5039527c9326053aed7411a8b65944ff971
Michaël <michael.gainyo@gmail.com>
2022-01-05 08:03:31 -0500

    updated fb pixel

--- miq/staff/serializers/page_ser.py
+++ miq/staff/serializers/page_ser.py
@@ -1,3 +1,4 @@
+from django.utils.text import slugify
 from django.contrib.sites.shortcuts import get_current_site
 
 from rest_framework import serializers
@@ -32,6 +33,9 @@ class PageSerializer(PageSerializerMixin):
     children = serializers.SlugRelatedField(
         slug_field="slug", read_only=True, many=True
     )
+
+    def validate_slug_public(self, value: str) -> str:
+        return slugify(value)
 
     def create(self, validated_data):
         validated_data.update({'site': get_current_site(self.get_request())})
