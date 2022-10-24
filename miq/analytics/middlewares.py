0147110bb0ffdb6fdbc8eaac3a793dae488be17e
Michaël <michael.gainyo@gmail.com>
2022-10-24 12:50:59 +0100

    updated middleware tracking

--- miq/analytics/middlewares.py
+++ miq/analytics/middlewares.py
@@ -1,9 +1,15 @@
-
+from django.utils.functional import SimpleLazyObject
 from django.contrib.sites.middleware import CurrentSiteMiddleware
 
-from .utils import create_hit
+from .utils import create_hit, create_visitor
 
 from ..honeypot.utils import is_threat
+
+
+def get_visitor(request):
+    if not hasattr(request, '_cached_visitor'):
+        request._cached_visitor = create_visitor(request)
+    return request._cached_visitor
 
 
 class AnalyticsMiddleware(CurrentSiteMiddleware):
@@ -11,12 +17,17 @@ class AnalyticsMiddleware(CurrentSiteMiddleware):
         self.get_response = get_response
 
     def __call__(self, request):
+
+        request.is_threat = is_threat(request)
+        request.visitor = SimpleLazyObject(lambda: get_visitor(request))
+
         response = self.get_response(request)
+
         if '/media/' in request.path:
             return response
 
         try:
-            create_hit(request, response, is_threat=is_threat(request))
+            create_hit(request, response)
         except Exception as e:
             print('\n\nError creating hit', e, '\n\n')
 
--- miq/analytics/utils.py
+++ miq/analytics/utils.py
@@ -1,9 +1,11 @@
 
+# from pprint import pprint
 import logging
 import datetime
 from urllib.parse import urlparse, parse_qs
 from user_agents import parse as _parse_
 
+from django.conf import settings
 from django.utils import timezone
 
 from ..core.utils import get_ip
@@ -19,14 +21,36 @@ logerr = logger.error
 exclude = ['/media/', '/favicon.ico', ]
 
 # SESSION APP KEYS
-cus_key = '_cus'
-cart_key = '_cart'
-cached_query_key = '_cqk'
+
+QUERY_CACHE_KEY = '_cqk'
+CUSTOMER_SESSION_KEY = getattr(settings, 'CUSTOMER_SESSION_KEY', '_cus')
+CART_SESSION_KEY = getattr(settings, 'CART_SESSION_KEY', '_cart')
+
 
 bots = [
     'bot', 'facebookexternalua', 'python', 'aiohttp', 'scrapy',
     'insomnia', 'expanse', 'linkwalker',
 ]
+
+
+def create_visitor(request, *, is_bot=False, ** kwargs):
+    ua = request.META.get('HTTP_USER_AGENT')
+    filter = {'ip': get_ip(request), 'user_agent': ua}
+    visitor = Visitor.objects.filter(**filter)
+    user = request.user if request.user.is_authenticated else None
+    if not visitor.exists():
+        visitor = Visitor.objects.create(
+            **filter, user=user,
+            is_bot=is_bot or get_hit_is_bot(ua, path=request.path)
+        )
+        loginfo(f'created visitor: {visitor}')
+        return visitor
+
+    visitor = visitor.order_by('-created').first()
+    if not visitor.user and request.user.is_authenticated:
+        visitor.user = user
+        visitor.save()
+    return visitor
 
 
 def get_hit_is_bot(user_agent, *, path=None):
@@ -102,27 +126,7 @@ def parse_hits():
     loginfo(f'parsed {hits.count()} hits')
 
 
-def create_visitor(request, *, response=None, is_bot=False):
-    ua = request.META.get('HTTP_USER_AGENT')
-    filter = {'ip': get_ip(request), 'user_agent': ua}
-    visitor = Visitor.objects.filter(**filter)
-    user = request.user if request.user.is_authenticated else None
-    if not visitor.exists():
-        visitor = Visitor.objects.create(
-            **filter, user=user,
-            is_bot=is_bot or get_hit_is_bot(ua, path=request.path)
-        )
-        loginfo(f'created visitor: {visitor}')
-        return visitor
-
-    visitor = visitor.order_by('-created').first()
-    if not visitor.user and request.user.is_authenticated:
-        visitor.user = user
-        visitor.save()
-    return visitor
-
-
-def create_hit(request, response, /, source: str = None, is_threat=False, ** kwargs) -> Hit:
+def create_hit(request, response, /, source: str = None, ** kwargs) -> Hit:
     if skip_hit(request):
         return
 
@@ -130,13 +134,13 @@ def create_hit(request, response, /, source: str = None, is_threat=False, ** kwa
 
     # CART
 
-    if cart := request.session.get(cart_key):
-        data['session_data'][cart_key] = cart
+    if cart := request.session.get(CART_SESSION_KEY):
+        data['session_data'][CART_SESSION_KEY] = cart
 
     # CUSTOMER
 
-    if cus := request.session.get(cus_key):
-        data['session_data'][cus_key] = cus
+    if cus := request.session.get(CUSTOMER_SESSION_KEY):
+        data['session_data'][CUSTOMER_SESSION_KEY] = cus
 
     source = source or request.session.get('source')
 
@@ -161,35 +165,19 @@ def create_hit(request, response, /, source: str = None, is_threat=False, ** kwa
     if source:
         data.update({'source_id': source})
 
-    cached_query = {
-        key: value for key, value in request.session.get(cached_query_key, {}).items()
-        if key not in ['r']
-    } or {}
-
-    ip = data.get('ip')
     url = data.get('url')
-    session = data.get('session')
-    path = data.get('path')
-    method = data.get('method')
-    site_id = data.get('site_id')
-    status = data.get('status')
-    referrer = data.get('referrer')
-    user_agent = data.get('user_agent')
 
-    query = {**cached_query, **parse_qs(urlparse(url).query)}
-    if query:
-        query = {key: value for key, value in query.items() if not key.startswith('__')}
-        request.session.update({cached_query_key: query})
+    if query := get_request_query(request, exclude=['r']):
+        request.session.update({QUERY_CACHE_KEY: query})
         data['session_data'].update({'query': query})
 
     last = Hit.objects.filter(
-        ip=ip, session=session, url=url, path=path,
-        method=method, site_id=site_id, response_status=status,
+        ip=data.get('ip'), session=data.get('session'), url=url, path=data.get('path'),
+        method=data.get('method'), site_id=data.get('site_id'), response_status=data.get('status'),
         created__gt=timezone.now() - datetime.timedelta(minutes=1),
     )
 
-    visitor = create_visitor(request, response=response)
-
+    visitor = request.visitor
     if last.exists() and (hit := last.order_by('-created').first()):
         # hit.count += 1
         hit.session_data = {**hit.session_data, **data}
@@ -202,13 +190,28 @@ def create_hit(request, response, /, source: str = None, is_threat=False, ** kwa
         hit = Hit.objects.create(**{
             **data,
             'visitor': visitor,
-            'parsed_data': parse_hit_data(url, referrer, user_agent, data.get('session_data', {})),
+            'parsed_data': parse_hit_data(url, data.get('referrer'), data.get('user_agent'), data.get('session_data', {})),
             'is_parsed': True,
             'is_bot': visitor.is_bot,
         })
         loginfo(f'new hit: {hit.slug}')
 
     return hit
+
+
+def get_request_query(request, *, exclude: list = None):
+    cached_query = {key: value for key, value in request.session.get(QUERY_CACHE_KEY, {}).items()}
+
+    query = {**cached_query, **parse_qs(urlparse(get_request_url(request)).query)}
+
+    _exclude = [key for key in query.keys() if key.startswith('__')]
+    if isinstance(exclude, list):
+        _exclude.extend(exclude)
+
+        for key in _exclude:
+            query.pop(key, None)
+
+    return query
 
 
 def get_hit_data(request, response, /, source: str = None) -> dict:
@@ -220,17 +223,11 @@ def get_hit_data(request, response, /, source: str = None) -> dict:
             logerr(f'error creating session\n{e}')
             return
 
-    url = request.build_absolute_uri() \
-        or request.get_full_path_info() \
-        or request.get_full_path() \
-        or request.path_info \
-        or request.path
-
     return {
         'site_id': request.site.id,
         'ip': get_ip(request),
         'session': request.session.session_key,
-        'url': url,
+        'url': get_request_url(request),
         'path': request.path,
         'method': request.method,
         'referrer': request.META.get('HTTP_REFERER'),
@@ -238,6 +235,14 @@ def get_hit_data(request, response, /, source: str = None) -> dict:
         'response_status': response.status_code,
         'session_data': {},
     }
+
+
+def get_request_url(request):
+    return request.build_absolute_uri() \
+        or request.get_full_path_info() \
+        or request.get_full_path() \
+        or request.path_info \
+        or request.path
 
 
 def skip_hit(request):
